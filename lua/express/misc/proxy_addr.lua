7c7eb51d2444d26e232686c780f9609153c7dcaf
_AMD_ <amd@default.im>
2023-11-21 09:40:31 +0300

    fix proxy_addr. ipv6 not supported

--- lua/express/misc/proxy_addr.lua
+++ lua/express/misc/proxy_addr.lua
@@ -2,9 +2,9 @@
 
 -- https://github.com/whitequark/ipaddr.js/blob/fb169615d04bb5f1c6042a07d85f9b09bef69af6/lib/ipaddr.js#L182-L211
 local IP_RANGES = {
-	linklocal   = {"169.254.0.0/16", "fe80::/10"},
-	loopback    = {"127.0.0.1/8", "::1/128"},
-	uniquelocal = {"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", "fc00::/7"}
+	linklocal   = {"169.254.0.0/16"}, -- "fe80::/10"
+	loopback    = {"127.0.0.1/8"}, -- "::1/128"
+	uniquelocal = {"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"} -- "fc00::/7"
 }
 
 local maskHasIP do
